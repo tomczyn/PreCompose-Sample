47a7a8e490f8da2d2fedda696e61b326910e222e
Maciej Tomczynski <maciej.tomczynski@kingfisher.com>
2024-04-23 14:44:53 +0200

    Animations showcase

--- composeApp/src/commonMain/kotlin/App.kt
+++ composeApp/src/commonMain/kotlin/App.kt
@@ -1,15 +1,21 @@
-import androidx.compose.foundation.background
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ModalBottomSheet
+import androidx.compose.material3.SheetState
+import androidx.compose.material3.TextField
 import androidx.compose.material3.rememberModalBottomSheetState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -17,7 +23,6 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import kotlinx.coroutines.launch
 import moe.tlaster.precompose.PreComposeApp
@@ -33,19 +38,45 @@ fun App() {
     PreComposeApp {
         MaterialTheme {
             Column(modifier = Modifier.fillMaxSize()) {
-                var showBottomSheet by remember { mutableStateOf(false) }
-                val navigator = rememberNavigator()
+                var showBottomSheet by remember { mutableStateOf(-1) }
+                val navigator = rememberNavigator("main")
+                val sheetState = rememberModalBottomSheetState(skipPartiallyExpanded = true)
+                val scope = rememberCoroutineScope()
                 NavHost(
                     navigator = navigator,
                     navTransition = NavTransition(),
                     initialRoute = "/home",
                 ) {
                     scene(route = "/home") {
-                        HomeView(showSheet = { showBottomSheet = true })
+                        Column(
+                            Modifier.fillMaxSize().padding(16.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally
+                        ) {
+                            Button(onClick = { showBottomSheet = 0 }) {
+                                Text("Show NavHost bottom sheet")
+                            }
+                            Spacer(Modifier.height(16.dp))
+                            Button(onClick = { showBottomSheet = 1 }) {
+                                Text("Show Non NavHost bottom sheet")
+                            }
+                        }
                     }
                 }
-                if (showBottomSheet) {
-                    BottomSheetView(hideBottomSheet = { showBottomSheet = false })
+                if (showBottomSheet != -1) {
+                    BottomSheetView(
+                        showNavHost = showBottomSheet == 0,
+                        sheetState = sheetState,
+                        hideBottomSheet = { showBottomSheet = -1 })
+                }
+                LaunchedEffect(showBottomSheet) {
+                    if (showBottomSheet != -1) {
+                        val type = if (showBottomSheet == 0) "NavHost" else "Non NavHost"
+                        println("Showing $type")
+                        scope.launch { sheetState.show() }
+                            .invokeOnCompletion {
+                                println("$type shown")
+                            }
+                    }
                 }
             }
         }
@@ -55,38 +86,31 @@ fun App() {
 @Composable
 @OptIn(ExperimentalMaterial3Api::class)
 private fun BottomSheetView(
+    showNavHost: Boolean,
+    sheetState: SheetState,
     hideBottomSheet: () -> Unit,
 ) {
-    val sheetState = rememberModalBottomSheetState()
     val scope = rememberCoroutineScope()
     ModalBottomSheet(
         sheetState = sheetState,
         onDismissRequest = hideBottomSheet,
-        dragHandle = null,
         content = {
-            BottomSheetContent {
-                scope.launch { sheetState.hide() }
-                    .invokeOnCompletion {
-                        if (!sheetState.isVisible) hideBottomSheet()
-                    }
+            val onHideBottomSheet: () -> Unit = remember {
+                {
+                    scope.launch { sheetState.hide() }
+                        .invokeOnCompletion {
+                            if (!sheetState.isVisible) hideBottomSheet()
+                        }
+                }
+            }
+            if (showNavHost) {
+                BottomSheetContent(onHideBottomSheet)
+            } else {
+                NoNavHostBottomSheetContent(onHideBottomSheet)
             }
         }
     )
 }
-
-@Composable
-private fun HomeView(showSheet: () -> Unit) {
-    Column(
-        Modifier.fillMaxSize().background(Color.Blue),
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text("Main Title")
-        Button(onClick = showSheet) {
-            Text("Show bottom sheet")
-        }
-    }
-}
-
 
 @Composable
 private fun BottomSheetContent(
@@ -94,20 +118,72 @@ private fun BottomSheetContent(
 ) {
     val navigator = rememberNavigator()
     NavHost(
+        modifier = Modifier.animateContentSize(),
         navigator = navigator,
         navTransition = NavTransition(),
-        initialRoute = "/sheet",
+        initialRoute = "/sheet1",
     ) {
-        scene(route = "/sheet") {
-            Column(
-                Modifier.fillMaxWidth().height(150.dp).background(Color.Red),
-                horizontalAlignment = Alignment.CenterHorizontally
-            ) {
-                Text("BottomSheet Title")
-                Button(onClick = onHideBottomSheet) {
-                    Text("Hide bottom sheet")
-                }
-            }
+        scene(route = "/sheet1") {
+            Content(
+                modifier = Modifier.fillMaxWidth().height(200.dp),
+                title = "NavHost 1",
+                onClick = { navigator.navigate("/sheet2") }
+            )
+        }
+        scene(route = "/sheet2") {
+            Content(
+                modifier = Modifier.fillMaxWidth().height(350.dp),
+                title = "NavHost 2",
+                onClick = { onHideBottomSheet() }
+            )
+        }
+    }
+    DisposableEffect(Unit) {
+        onDispose { navigator.popBackStack() }
+    }
+}
+
+@Composable
+private fun NoNavHostBottomSheetContent(
+    onHideBottomSheet: () -> Unit,
+) {
+    Column(
+        modifier = Modifier.animateContentSize(),
+    ) {
+        var sheet2 by remember { mutableStateOf(false) }
+        if (!sheet2) {
+            Content(
+                modifier = Modifier.fillMaxWidth().height(200.dp),
+                title = "Non NavHost 1",
+                onClick = { sheet2 = true }
+            )
+        } else {
+            Content(
+                modifier = Modifier.fillMaxWidth().height(350.dp),
+                title = "Non NavHost 2",
+                onClick = { onHideBottomSheet(); sheet2 = false }
+            )
+        }
+    }
+}
+
+@Composable
+private fun Content(
+    modifier: Modifier,
+    title: String,
+    onClick: () -> Unit,
+) {
+    Column(
+        modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        var field by remember { mutableStateOf("") }
+        Text(title)
+        TextField(
+            value = field,
+            onValueChange = { field = it })
+        Button(onClick = { onClick() }) {
+            Text("Next")
         }
     }
 }
